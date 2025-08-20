2519cb11251070c8336c298fe90359ea1a8f4d0d
Kevin <kevin.e.mccarthy1983@gmail.com>
2025-08-20 17:42:25 +0100

    fixing issue with table creation

--- backend/app/core/auth/auth_service.py
+++ backend/app/core/auth/auth_service.py
@@ -316,7 +316,7 @@ class AuthService:
             value=session_id,
             httponly=settings.auth.http_only,
             max_age=600,
-            samesite="lax",
+            samesite=settings.auth.same_site,
             secure=settings.app.environment == "production",
             domain=settings.auth.domain,
         )
@@ -354,7 +354,7 @@ class AuthService:
             value=session_id,
             httponly=app_settings.auth.http_only,
             max_age=600,
-            samesite="lax",
+            samesite=app_settings.auth.same_site,
             secure=True,
             domain=app_settings.auth.domain,
             path="/",
